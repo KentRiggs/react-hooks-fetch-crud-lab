cc902cdc3bcaa8ef05a8c76c7ac0219439ffb9b3
kent riggs <k2r3@proton.me>
2024-02-11 19:27:15 -0700

    added code to satisfy tests

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, updateQuestions, deleteQuestion}) {
+  
+  useEffect(() => {
+    fetch("http://localhost:4000/questions/")
+    .then(r => r.json())
+    .then(dbQuestions => updateQuestions(dbQuestions));
+  }, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question => <QuestionItem key={question.id} question={question} deleteQuestion={deleteQuestion} />)}
+      </ul>
     </section>
   );
 }
